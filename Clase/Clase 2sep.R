9744a13a421c2d117b44ba080ea113c942834ea7
Alelizondot <alejandra.elizondoto@uanl.edu.mx>
2024-09-02 10:51:13 -0600

    Clase 2 - Uso de restricciones, funciones which y subconjunto [,]

---
+++ Clase/Clase 2sep.R
@@ -0,0 +1,32 @@
+conjunto<- read.csv("C:/Repositorios/Met_Est_2024/Tareas/DATOS_HW02.csv")
+
+conjunto$Especie<- as.factor (conjunto$Especie)
+
+boxplot(conjunto$Diametro ~ conjunto$Especie,
+        xlab= "Especie",
+        ylab= "Diametro (cm)",
+        col="blue")
+
+boxplot(conjunto$Altura ~ conjunto$Especie,
+        xlab= "Especie",
+        ylab= "Diametro (cm)",
+        col="blue")
+#seleccionar la especie F mediante subset (Subconjunto)
+SpF<- subset (conjunto, conjunto$Especie == "F")
+
+#Seleccionar exceptuando F
+SpHC <- subset(conjunto, conjunto$Especie != "F")
+
+#
+tapply(conjunto$Altura,conjunto$Especie, mean)
+
+tapply(conjunto$Diametro,conjunto$Especie, mean)
+ 
+#hacer un subset donde sean mayores o iguales a la media de la altura
+SpHpro <- subset(conjunto, conjunto$Altura >= mean(conjunto$Altura))
+
+#hacer un subset donde sean menores o iguales a la media de la altura
+SpLpro <- subset(conjunto, conjunto$Altura <= mean(conjunto$Altura))
+
+#saber el valor atipico de la boxplot de altura
+which(conjunto$Altura>20)
