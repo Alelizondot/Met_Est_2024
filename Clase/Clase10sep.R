8533fc9c40c95adab9b344c3f023acbe128e983e
Alelizondot <alejandra.elizondoto@uanl.edu.mx>
2024-09-10 09:19:27 -0600

    Pruebas con los cubos de madera

---
+++ Clase/Clase10sep.R
@@ -0,0 +1,3 @@
+#importar datos de github
+read.csv("https://raw.githubusercontent.com/mgtagle/Met_Est_2024/main/Datos_Madera_MET.csv")
+madera<-read.csv("https://raw.githubusercontent.com/mgtagle/Met_Est_2024/main/Datos_Madera_MET.csv", header = T)
